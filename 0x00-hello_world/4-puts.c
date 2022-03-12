00bf81530abbc401eded09edf1d338ca873989b8
NaomiMariga <kaburamariga@gmail.com>
2022-03-12 03:24:30 -0800

    Take care of betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main()
+/*
+ *main - entry point
+ *
+ *Return - always 0
+ */
+
+int main(void)
 {
+	/*Use puts to output to the stdout*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
