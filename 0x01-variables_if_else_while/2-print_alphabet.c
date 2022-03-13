5f6d723739337bffe25f2006b24ef18cb9ab80be
NaomiMariga <kaburamariga@gmail.com>
2022-03-13 01:42:13 -0800

    Last lowercase a - z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int lower = 97;
+
+	while (lower <= 122)
+	{
+		char c = lower;
+
+		putchar(c);
+		lower++;
+	}
+	return (0);
+}
