db36416d82e8edce0fc5fa917206f452d85addb7
NaomiMariga <kaburamariga@gmail.com>
2022-03-15 21:13:27 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0 on success 
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar('\n');
+	return (0);
+}
