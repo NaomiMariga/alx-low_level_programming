303c92d6d634dd0c45f921eb3a36adf57049a370
NaomiMariga <kaburamariga@gmail.com>
2022-03-20 00:46:19 -0700

    Print lowercase a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lower case a - z
+ */
+void print_alphabet(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		_putchar(lower);
+	}
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 int printChars(char letters[]);
+void print_alphabet(void);
