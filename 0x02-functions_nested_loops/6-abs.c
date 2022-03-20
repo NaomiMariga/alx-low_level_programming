66eeba1c62d2a3fb688a7dd85914cd597ecb3230
NaomiMariga <kaburamariga@gmail.com>
2022-03-20 03:00:54 -0700

    absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - changes nagatives to positive numbers
+ *
+ * @n: number to be changed
+ *
+ * Return: positive n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
