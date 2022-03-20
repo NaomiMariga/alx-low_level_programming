8d447b630873953d3f4b4ac5b2281c7c39a103b6
NaomiMariga <kaburamariga@gmail.com>
2022-03-20 02:39:52 -0700

    print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print-sign - print signs if given conditions are met
+ *
+ * Return: 1 if true and 0 if false
+ *
+ * @n: value to be checked
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
+
