089710beb3bdd0d11da3328c474119e48b74aa93
NaomiMariga <kaburamariga@gmail.com>
2022-03-19 23:04:50 -0700

    Print characters using _putchar custom function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,32 @@
 #include <unistd.h>
 #include <stdio.h>
+#include "main.h"
+#include <string.h>
 
 /**
  * main - print _putchar
  *
- * Return: Always 0 on success 
+ * Return: Always 0 on success
  */
+
 int main(void)
 {
-	_putchar("_putchar");
+	printChars();
 	_putchar('\n');
+	return (0);
+}
+
+int printChars(void)
+{
+	char letters[] = "_putchar";
+	unsigned long int pos = 0;
+
+	while (pos < strlen(letters))
+	{
+		char letter = letters[pos];
+
+		_putchar(letter);
+		pos++;
+	}
 	return (0);
 }
