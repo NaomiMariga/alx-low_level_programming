1b376505a3c5e26dd4bca2d743cfdc1d53dec6c2
NaomiMariga <kaburamariga@gmail.com>
2022-03-20 05:50:02 -0700

    Fix: use break

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,18 +12,26 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 2; hour++)
 	{
-		for (hours = 0; hours <= 3; hours++)
+		for (hours = 0; hours <= 9; hours++)
 		{
 			for (min = 0; min <= 5; min++)
 			{
 				for (mins = 0; mins <= 9; mins++)
 				{
-					_putchar('0' + hour);
-					_putchar('0' + hours);
-					_putchar(':');
-					_putchar('0' + min);
-					_putchar('0' + mins);
-					_putchar('\n');
+					if (hour == 2 && hours == 4 && min == 0 && mins == 0)
+					{
+						break;
+					}
+					else
+					{
+						_putchar('0' + hour);
+						_putchar('0' + hours);
+						_putchar(':');
+						_putchar('0' + min);
+						_putchar('0' + mins);
+						_putchar('\n');	
+					}
+				
 				}
 			}
 		}
