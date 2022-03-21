103bb3572076c3ae7d370ed3fafe2972ac2f2fbd
NaomiMariga <kaburamariga@gmail.com>
2022-03-21 07:43:21 -0700

    Printing commas and spaces before the result

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * times_table - prints 9 times table
  */
@@ -12,33 +11,33 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (k <= 9 && j < 9)
-			{
+			if (j == 0)
 				_putchar(k + '0');
+			if (k <= 9 && j < 9 && j != 0)
+			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar(k + '0');
 			}
 			else if (k <= 9 && j == 9)
 			{
-				/**
-				 * 9 is the last number so we are checking so as not to put comma and space
-				 * Instead we print a new line when we print the last product
-				 * Also where result is more than 1 digit we divide by 10
-				 * then get modulo to get 2 characters
-				 */
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(k + '0');
 				_putchar('\n');
 			}
-			else if (k >= 10 && j < 9)
-			{
+			else if (k >= 10 && j < 9 && j != 0)
+			{	_putchar(',');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
 			else if (k >= 10 && j == 9)
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 				_putchar('\n');
