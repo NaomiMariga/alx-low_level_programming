fe7e84a250eaac0bc471a6bc9a4154e872ae0f1f
NaomiMariga <kaburamariga@gmail.com>
2022-03-21 16:29:53 -0700

    string reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - reverse a string
+ *
+ * @s: string to reversed
+ */
+void print_rev(char *s)
+{
+	int index;
+	char letter;
+
+	for (index = -1; index < -(strlen(s)); index--)
+	{
+		letter = s[index];
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
