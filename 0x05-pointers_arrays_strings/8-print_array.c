be2e92930f80766dcfa7742e907d5317cd6dc2be
NaomiMariga <kaburamariga@gmail.com>
2022-03-22 06:54:23 -0700

    print array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints array items
+ *
+ * @a: the int array
+ *
+ * @n: number of item to be printed
+ */
+void print_array(int *a, int n)
+{
+	int index;
+	int item;
+
+	for (index = 0; index < n; index++)
+	{
+		item = a[index];
+		printf("%d, ", item);
+	}
+}
