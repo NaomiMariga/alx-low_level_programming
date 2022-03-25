7b2a63acb78abd38e4500f27ab9f0235d4a614fc
NaomiMariga <kaburamariga@gmail.com>
2022-03-25 10:12:59 -0700

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compare 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: differnce in strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	unsigned long int index;
+	char s1_letter;
+	char s2_letter;
+	int difference;
+
+	for (index = 0; index < strlen(s1) || index < strlen(s2); index++)
+	{
+		s1_letter = si[index];
+		s2_letter = s2[index];
+
+		if (s1_letter != s2_letter)
+		{
+			difference = s1_letter - s2_letter;
+		}
+		else
+		{
+			difference = s1_letter - s2_letter;
+		}
+	}
+	return (difference);
+}
