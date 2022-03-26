2b3077e449032f169d3290b0d4d7acafdbd8b43f
NaomiMariga <kaburamariga@gmail.com>
2022-03-26 04:02:49 -0700

    try: change some letters

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * leet - replace  some letters with numbers
+ *@s: string to be manipulated
+ * Return: manipulated string
+ */
+char *leet(char *s)
+{
+	unsigned long int index;
+	char letter;
+
+	for (index = 0; index < strlen(s); index++)
+	{
+		letter = s[index];
+		if (letter == 'a' || letter == 'A')
+		{
+			s[index] = '4';
+		}
+		else if (letter == 'e' || letter == 'E')
+		{
+			s[index] = '3';
+		}
+		else if (letter == 'o' || letter == 'O')
+		{
+			s[index] = '0';
+		}
+		else if (letter == 't' || letter == 'T')
+		{
+			s[index] = '7';
+		}
+		else if (letter == 'l' || letter == 'L')
+		{
+			s[index] = '1';
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ void reverse_array(int *a, int n);
 int _putchar(char c);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
