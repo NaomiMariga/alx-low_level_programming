4abea8f9c78424ae72009fac48f5392aa39f1b6a
NaomiMariga <kaburamariga@gmail.com>
2022-03-26 08:42:54 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints _ each time condition is true
+ * @n: number of _ to be printed
+ */
+
+void print_line(int n)
+{
+	int count;
+
+	for (count = 0; count <= n; count++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
