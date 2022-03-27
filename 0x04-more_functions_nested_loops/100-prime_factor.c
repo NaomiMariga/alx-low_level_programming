dc640445bc8af30783024d29817966028c08bc64
NaomiMariga <kaburamariga@gmail.com>
2022-03-27 11:16:47 -0700

    print numbers using recursion

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print primes
+ * Return: Always on 0
+ */
+
+int main()
+{
+	int x;
+
+	x = 612852475143;
+	
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers using putchar
+ * @n: number to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
