b64ddd94f08f5fa4f6bb276adcf994eba3d0f263
NaomiMariga <kaburamariga@gmail.com>
2022-04-04 14:57:11 -0700

    multiplication from commandline args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of commandline arguements
+ * @argc: number of args
+ * @argv: list of args
+ * Return: always 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int i;
+		int prod = 1;
+
+		for (i = 1; i < argc; i++)
+		{
+			prod *= atoi(argv[i]);
+		}
+	printf("%d\n", prod);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
