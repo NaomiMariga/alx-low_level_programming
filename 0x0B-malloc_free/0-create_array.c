100cac8887342b9bc0714b1cca5caee3644e26d0
NaomiMariga <kaburamariga@gmail.com>
2022-04-23 15:10:45 -0700

    use while

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,22 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	str = malloc(sizeof(c) * size);
-	str[0] = c;
+	str = malloc(sizeof(char) * size);
 	if (str == NULL)
 	{
 		return (NULL);
+	}
+	i = 0;
+	while (i <= size)
+	{
+		str[i] = c;
+		i++;
 	}
 	return (str);
 }
